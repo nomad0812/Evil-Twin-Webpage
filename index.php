f7a3c86a3eb8a11840a03cd8dc78636d12408ee0
root <root@Ghostdrive.localdomain>
2018-06-11 01:43:26 -0400

    Added php

---
+++ index.php
@@ -0,0 +1,27 @@
+<head>
+  <meta charset="utf-8"/> 
+  <title>Re-authentication</title>
+  <link rel="stylesheet" type="text/css" href="abel.css">
+  <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+    <section id="header">
+      <img src="lock.png" id="lock_ico" height="150" width="150" />
+      <br/><br/>
+      <span id="greeting">Please re-authenticate your password</span>
+      <br/>
+      <br/>
+      <br/>
+      <div id="p1">
+      <input class="pwdBox" id="pwd1" type="text" value="Password"/>
+    </div>
+      <br/>
+      <div id="p2">
+      <input class="pwdBox" id="pwd2" type="text" value="Re-enter password"/>
+    </div>
+    <br/>
+    <div id="upload">
+      <button id="uploadBut" type="submit">Submit</button>
+    </div>
+    </section>
+</body>
